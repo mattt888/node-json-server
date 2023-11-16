c80fad11a0d0d3234b99f730fc668781734c09fe
appworld-hu <burai.sandor@gmail.com>
2023-11-16 07:44:41 +0100

    body parser added, post data available

--- index.js
+++ index.js
@@ -2,13 +2,18 @@ const express = require('express');
 const data = require('./data')
 const cors = require('cors')
 const app = express()
+const bodyParser = require('body-parser')
 
 
 app
 .use(cors())
+.use(bodyParser.json({extended: true}))
+.use(bodyParser.urlencoded({extended: true}))
 .get('/',(req, res)=>{
-    res.writeHead(200, {'COntent-Type': 'application/json'})
+    res.writeHead(200, {'Content-Type': 'application/json'})
     res.end( data )
 })
-
+.post('/', (req, res)=>{
+    res.end( JSON.stringify(req.body) )
+})
 .listen(3000)
